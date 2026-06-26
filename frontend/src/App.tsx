ec3d31bbe77b0ebf833296355a0fb83ce8f7f1ad
Dianagarcia33 <41555299+Dianagarcia33@users.noreply.github.com>
2026-06-26 14:52:17 -0500

    Fix maintenance mode logic in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { LandingPage } from "./features/landing/components/LandingPage"
 import { MaintenancePage } from "./features/maintenance/components/MaintenancePage"
 
 function App() {
-  // Leemos la variable de entorno de Vite
   const isMaintenanceMode = import.meta.env.VITE_MAINTENANCE_MODE === 'true';
 
   if (isMaintenanceMode) {
@@ -14,7 +13,7 @@ function App() {
     <>
       <LandingPage />
     </>
-  )
+  );
 }
 
 export default App;
